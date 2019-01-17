de496036b9d461462dcdcb2b5378938be0315a47
Darrin Howell <dbhowell09@gmail.com>
2019-01-17 10:58:32 -0800

    spring boot app running

---
+++ src/main/java/com/darrinhowell/healthtrackerbackend/Exercise.java
@@ -0,0 +1,28 @@
+package com.darrinhowell.healthtrackerbackend;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id;
+    public String title;
+    public String quantity;
+    public String description;
+    public String timeStamp;
+
+    public Exercise() {}
+
+    public Exercise(String title, String quantity, String description, String timeStamp) {
+        this.title = title;
+        this.quantity = quantity;
+        this.description = description;
+        this.timeStamp = timeStamp;
+    }
+
+}
---
+++ src/main/java/com/darrinhowell/healthtrackerbackend/ExerciseController.java
@@ -0,0 +1,8 @@
+package com.darrinhowell.healthtrackerbackend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ExerciseController {
+
+    @Autowired ExerciseRepository exerciseRepo;
+}
